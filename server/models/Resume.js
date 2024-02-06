b4a00b9aa3707709024851aa1dc06ce01fe84108
KC-Nick <wilson.kasinichole@gmail.com>
2024-02-05 20:08:14 -0500

    user model rought draft

--- server/models/Resume.js
+++ server/models/Resume.js
@@ -4,14 +4,17 @@ const ExperienceSchema = require('./Experience');
 const SkillSchema = require('./Skill');
 
 const ResumeSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  education: [EducationSchema],
-  experience: [ExperienceSchema],
-  skills: [SkillSchema],
+    opener: {
+        type: String,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    education: [EducationSchema],
+    experience: [ExperienceSchema],
+    skills: [SkillSchema],
 });
 
 module.exports = model('Resume', ResumeSchema);
