7d8e510fc48194df39b44d4ad4b3c5534acef150
Nic <139719585+KC-Nick@users.noreply.github.com>
2024-02-05 20:11:05 -0500

    user model rough draft (#9)
    * vite react commit
    
    * adding files to server folder
    
    * adding boilerplate code to server file
    
    * little changes
    
    * resume, education, skills, and experience models rough draft
    
    * user model rought draft

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,41 @@
 const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    resumes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Resume',
+    }],
+});
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
