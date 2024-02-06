d0dc0d0a0dbe707842f8bec02d3a53b5a9cbf223
KC-Nick <wilson.kasinichole@gmail.com>
2024-02-05 19:10:05 -0500

    little changes

--- server/models/Skills.js
+++ server/models/Skills.js
@@ -1,1 +1,22 @@
 const { Schema, model } = require('mongoose');
+
+const mongoose = require('mongoose');
+
+const SkillSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  proficiency: {
+    type: String,
+    enum: ['Beginner', 'Intermediate', 'Advanced', 'Expert'],
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Skill', SkillSchema);
