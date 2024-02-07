788c8af229ab2fde46e15fe391d893d8c2b09aa1
Nic <139719585+KC-Nick@users.noreply.github.com>
2024-02-07 13:52:25 -0500

    forgot to take out commented out code in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -19,24 +19,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-
-// import Home from '../src/pages/Home';
-// import Login from '../src/pages/Login';
-// import ResumeApp from '../src/components/ResumeApp';
-// import LandingPage from '../src/components/Landing';
-// import Signup from '../src/pages/Signup';
-
-// function MainRoutes() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<App />}>
-//         <Route index element={<LandingPage />} />
-//         <Route path="login" element={<Login />} />
-//         <Route path="signup" element={<Signup />} />
-//         <Route path="resume" element={<ResumeApp />} />
-//         <Route path="home" element={<Home />} />
-//       </Route>
-//       <Route path="*" element={<h1 className='display-2'>Wrong page!</h1>} />
-//     </Routes>
-//   );
-// }
